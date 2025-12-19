57276a9affb7fa9862a644434a889c6136b1c2f7
appcode2022 <appcode2022@163.com>
2025-12-19 14:18:41 +0800

    add config schema

--- src/smithery.ts
+++ src/smithery.ts
@@ -1,18 +1,21 @@
 import {z} from "zod";
 import {server, isLoading} from "./xiaobenyang_mcp_tools.js";
 
+// export const configSchema = z.object({
+// })
+
 export const configSchema = z.object({
-})
+    apiKey: z.string().describe("Your API key"),
+    mcpId: z.string().describe("Model to use"),
+});
 
 export default function createServer({config,}: { config: z.infer<typeof configSchema> }) {
-    console.log("4444444");
 
     while (!isLoading) {
-        console.log("55555");
         setTimeout(() => {
             console.log('500 毫秒后执行');
         }, 500);
     }
-    console.log("5555");
+
     return server.server;
 }
